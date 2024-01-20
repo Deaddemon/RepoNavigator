2f1cf8177d9c672acaa5371f09966fe9ba12255f
vinita poonia <vinitapoonia333@gmail.com>
2024-01-21 00:27:57 +0530

    refactor: implemented js good practices

--- index.js
+++ index.js
@@ -77,33 +77,31 @@ function displayUserRepositories() {
 
     const repositoryList = document.getElementById('repositories');
     const repositoryTemplate = document.getElementById('repoTemplate');
+    repositoryList.innerHTML = ''; 
     
 
-    for (let i = startIndex; i < endIndex && i < repositoriesData.length; i++) {
+    repositoriesData.slice(startIndex, endIndex).forEach((repo) => {
 
-        
-        const  repository = repositoryTemplate.cloneNode(true);
+        const repository = repositoryTemplate.cloneNode(true);
         repository.style.display = 'block';
-
-        let repoTitle = repository.querySelector('.repoTitle');
-        let repoDescription = repository.querySelector('.repoDescription');
-        let repoTopics = repository.querySelector('.repoTopics');
-        
-
-        repoTitle.innerHTML = repositoriesData[i].name;
-        repoDescription.innerHTML = repositoriesData[i].description;
-        
-
-        for (let topic in repositoriesData[i].topics) {
-            let button = document.createElement('button');
-            button.classList.add('btn', 'btn-outline-info', 'btn-sm', 'm-1', 'disabled');
-            button.innerHTML = repositoriesData[i].topics[topic];
-            repoTopics.appendChild(button);
-        }
-        
-
+      
+        const repoTitle = repository.querySelector('.repoTitle');
+        const repoDescription = repository.querySelector('.repoDescription');
+        const repoTopics = repository.querySelector('.repoTopics');
+      
+        repoTitle.innerHTML = repo.name;
+        repoDescription.innerHTML = repo.description;
+      
+        repo.topics.forEach(topic => {
+          const button = document.createElement('button');
+          button.classList.add('btn', 'btn-outline-info', 'btn-sm', 'm-1', 'disabled');
+          button.innerHTML = topic;
+          repoTopics.appendChild(button);
+        });
+      
         repositoryList.appendChild(repository);
-    }
+      });
+      
 }
 
 
@@ -141,47 +139,4 @@ function nextPage() {
 
 
 
-
-
-
-
-// function displayUserRepositories() {
-
-
-   
-//     let repositories = document.getElementById('repositories');
-//     repositories.innerHTML = '';
-
-//     const startIndex = (currentPage - 1) * reposPerPage;
-//     const endIndex = startIndex + reposPerPage;
-
-//     for (let i = startIndex; i < endIndex && i < repositoriesData.length; i++) {
-//         let figure = document.createElement('figure');
-//         let repositoryInfo = document.createElement('div');
-//         let title = document.createElement('h2');
-//         let figcaption = document.createElement('figcaption');
-//         let repositoryTopics = document.createElement('div');
-
-//         title.classList.add('repositoryName');
-//         title.innerHTML = repositoriesData[i].name;
-
-//         repositoryInfo.classList.add('repositoryInfo');
-
-//         figcaption.innerHTML = repositoriesData[i].description;
-
-//         for (let topic in repositoriesData[i].topics) {
-//             let button = document.createElement('button');
-//             button.classList.add('btn', 'btn-outline-info', 'btn-sm', 'm-1', 'disabled');
-//             button.innerHTML = repositoriesData[i].topics[topic];
-//             repositoryTopics.appendChild(button);
-//         }
-
-//         repositoryInfo.appendChild(title);
-//         repositoryInfo.appendChild(figcaption);
-
-//         figure.appendChild(title);
-//         figure.appendChild(figcaption);
-//         figure.appendChild(repositoryTopics);
-//         repositories.appendChild(figure);
-//     }
-// }
+ 
